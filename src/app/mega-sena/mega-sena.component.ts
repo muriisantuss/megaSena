1a9e9b9965382a2b4cef40f72e43cd9ed4d778f4
muriisantuss <muriisantuss@gmail.com>
2024-03-18 23:14:41 -0300

    function 'megaSena'

--- src/app/mega-sena/mega-sena.component.ts
+++ src/app/mega-sena/mega-sena.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-mega-sena',
   templateUrl: './mega-sena.component.html',
-  styleUrl: './mega-sena.component.css'
+  styleUrl: './mega-sena.component.css',
 })
 export class MegaSenaComponent {
+  number: number[] = [0,0,0,0,0,0];
 
+  megaSena() {
+    for (var a = 0; a < 6; a++) {
+      this.number[a] = Math.floor(Math.random() * 61);
+    }
+  }
 }
