e201d319d9ba6e883ca61306dc41eaac26314cef
muriisantuss <muriisantuss@gmail.com>
2024-03-20 00:52:09 -0300

    add function bet() and inputs for users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,10 +11,12 @@ import { MegaSenaComponent } from './mega-sena/mega-sena.component';
   declarations: [
     AppComponent,
     MegaSenaComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
--- src/app/mega-sena/mega-sena.component.ts
+++ src/app/mega-sena/mega-sena.component.ts
@@ -7,11 +7,20 @@ import { elementAt } from 'rxjs';
   styleUrl: './mega-sena.component.css',
 })
 export class MegaSenaComponent {
-  number: number[] = [0,0,0,0,0,0];
+  number: number[] = [];
+  catch: number[] = [];
+  validation: any;
 
   megaSena() {
     for (var a = 0; a < 6; a++) {
       this.number[a] = Math.floor(Math.random() * 61);
     }
+  }
+
+  hits() {
+    var arr1 = this.number;
+    var arr2 = this.catch;
+    let intersection = arr1.filter((x) => arr2.includes(x));
+    this.validation = intersection.length;
   }
 }
